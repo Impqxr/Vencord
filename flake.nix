8a4bdcf80874b18de7f07c4ac08d882da2c92b3b
Viktor Fedorov <impqxr@gmail.com>
2025-04-10 18:06:35 +0200

    bump tp 1.11.9

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
                 ({ buildWebExtension, userplugins }: stdenv.mkDerivation
                   (finalAttrs: {
                     pname = "vencord";
-                    version = "1.11.8";
+                    version = "1.11.9";
 
                     src = ./.;
 
